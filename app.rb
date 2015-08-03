766f7644fd35bf6721fc617366f106b2e1354491
Emily Yu <ey100101@gmail.com>
2015-08-03 16:54:32 -0400

    finish tutorial

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
 require 'sinatra'
+require 'sinatra/activerecord'
+require './config/environments' #database configuration
+require './models/model'
 
 get '/' do
-	"Hello, World!"
+	erb :index
 end
 
+post '/submit' do
+	@model = Model.new(params[:model])
+	if @mode1.save
+		redirect '/models'
+	else
+		"Sorry, there was an error!"
+	end
+end
+
+get '/models' do
+	@models = Model.all
+	erb :models
+ends
